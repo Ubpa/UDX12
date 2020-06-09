2335fa1fb550e73b4d46067436e6c0a22cee0b55
Ubpa <641614112@qq.com>
2020-06-09 10:49:59 +0800

    Desc::PSO::Basic

--- include/UDX12/Desc.h
+++ include/UDX12/Desc.h
@@ -6,4 +6,14 @@ namespace Ubpa::DX12::Desc {
     namespace SRC {
         D3D12_SHADER_RESOURCE_VIEW_DESC Tex2D(ID3D12Resource* pResource);
     }
+
+    namespace PSO {
+        D3D12_GRAPHICS_PIPELINE_STATE_DESC Basic(
+            ID3D12RootSignature* rootSig,
+            D3D12_INPUT_ELEMENT_DESC* pInputElementDescs, UINT NumElements,
+            ID3DBlob* VS,
+            ID3DBlob* PS,
+            DXGI_FORMAT rtvFormat = DXGI_FORMAT_R8G8B8A8_UNORM,
+            DXGI_FORMAT dsvFormat = DXGI_FORMAT_D24_UNORM_S8_UINT);
+    }
 }
--- src/core/Desc.cpp
+++ src/core/Desc.cpp
@@ -13,3 +13,33 @@ D3D12_SHADER_RESOURCE_VIEW_DESC DX12::Desc::SRC::Tex2D(ID3D12Resource* pResource
     srvDesc.Texture2D.ResourceMinLODClamp = 0.0f;
     return srvDesc;
 }
+
+D3D12_GRAPHICS_PIPELINE_STATE_DESC DX12::Desc::PSO::Basic(
+    ID3D12RootSignature* rootSig,
+    D3D12_INPUT_ELEMENT_DESC* pInputElementDescs, UINT NumElements,
+    ID3DBlob* VS,
+    ID3DBlob* PS,
+    DXGI_FORMAT rtvFormat,
+    DXGI_FORMAT dsvFormat)
+{
+	D3D12_GRAPHICS_PIPELINE_STATE_DESC psoDesc;
+
+	ZeroMemory(&psoDesc, sizeof(D3D12_GRAPHICS_PIPELINE_STATE_DESC));
+
+	psoDesc.InputLayout = { pInputElementDescs, NumElements };
+	psoDesc.pRootSignature = rootSig;
+	psoDesc.VS = { reinterpret_cast<BYTE*>(VS->GetBufferPointer()), VS->GetBufferSize() };
+	psoDesc.PS = { reinterpret_cast<BYTE*>(PS->GetBufferPointer()), PS->GetBufferSize() };
+	psoDesc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
+	psoDesc.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
+	psoDesc.DepthStencilState = CD3DX12_DEPTH_STENCIL_DESC(D3D12_DEFAULT);
+	psoDesc.SampleMask = UINT_MAX;
+	psoDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+	psoDesc.NumRenderTargets = 1;
+	psoDesc.RTVFormats[0] = rtvFormat;
+	psoDesc.SampleDesc.Count = 1;
+	psoDesc.SampleDesc.Quality = 0;
+	psoDesc.DSVFormat = DXGI_FORMAT_D24_UNORM_S8_UINT;
+
+	return psoDesc;
+}
